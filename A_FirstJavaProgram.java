232b90ae025fbc9fc4b126bfe46ff55bbabddccd
ikarus920214 <ikarus920214@gmail.com>
2019-07-04 15:53:10 +0900

    주석 변경

--- A_FirstJavaProgram.java
+++ A_FirstJavaProgram.java
@@ -4,7 +4,7 @@ public class A_FirstJavaProgram
 
 	public static void main(String[] args) 
 	{
-		// 한글 테스트
+		// 한글 테스트 2
 		System.out.println("환영합니다 Java");
 		System.out.println("First Java Program");
 		System.out.println(3 + 5);
