21222031d47f736c1e2db5f3c49e53c1545612d2
ikarus920214 <ikarus920214@gmail.com>
2019-07-04 15:44:59 +0900

    한글출력

--- A_FirstJavaProgram.java
+++ A_FirstJavaProgram.java
@@ -5,7 +5,7 @@ public class A_FirstJavaProgram
 	public static void main(String[] args) 
 	{
 		// 한글 테스트
-		System.out.println("Welcome to Java");
+		System.out.println("환영합니다 Java");
 		System.out.println("First Java Program");
 	}
 
